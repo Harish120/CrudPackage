9ef5bf94fa884733cb23a2739b8cc57b4184363b
Harish Pant <harish.pant@sociair.com>
2024-08-06 16:51:03 +0545

    :zap: namespace fetching

--- src/Console/Commands/RouteGenerator.php
+++ src/Console/Commands/RouteGenerator.php
@@ -15,7 +15,7 @@ class RouteGenerator
 
     public function generate($modelName)
     {
-        $route = "Route::apiResource('".strtolower(Str::plural($modelName))."', 'App\Http\Controllers\Api\{$modelName}Controller');";
+        $route = "Route::apiResource('".strtolower(Str::plural($modelName))."', 'App\Http\Controllers\Api\'{$modelName}Controller');";
         file_put_contents(base_path('routes/api.php'), $route.PHP_EOL, FILE_APPEND);
     }
 }
