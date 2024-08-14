986ef59ec83430f11af5a016056527948737bc80
Harish Pant <harish.pant@sociair.com>
2024-08-14 11:41:52 +0545

    :zap: modular migration columns

--- src/Helpers/ColumnValidator.php
+++ src/Helpers/ColumnValidator.php
@@ -12,8 +12,8 @@ class ColumnValidator
      */
     public static function validateColumnsFormat(string $columns): array
     {
-        // Define the pattern to match Laravel-supported data types with optional * and default values
-        $columnPattern = '/^(\w+:(bigIncrements|bigInteger|binary|boolean|char|date|dateTime|decimal|double|enum|float|geometry|geometryCollection|increments|integer|ipAddress|json|jsonb|lineString|longText|macAddress|mediumIncrements|mediumInteger|mediumText|morphs|multiLineString|multiPoint|multiPolygon|nullableMorphs|nullableTimestamps|point|polygon|rememberToken|set|smallIncrements|smallInteger|softDeletes|softDeletesTz|string|text|time|timeTz|timestamp|timestampTz|tinyIncrements|tinyInteger|tinyText|unsignedBigInteger|unsignedDecimal|unsignedInteger|unsignedMediumInteger|unsignedSmallInteger|unsignedTinyInteger|uuid|year)(\*?)(\s*=\s*[^,]*)?)$/';
+        // Define the pattern to match Laravel-supported data types with optional ? and * with default values
+        $columnPattern = '/^(\w+:(bigIncrements|bigInteger|binary|boolean|char|date|dateTime|decimal|double|enum|float|geometry|geometryCollection|increments|integer|ipAddress|json|jsonb|lineString|longText|macAddress|mediumIncrements|mediumInteger|mediumText|morphs|multiLineString|multiPoint|multiPolygon|nullableMorphs|nullableTimestamps|point|polygon|rememberToken|set|smallIncrements|smallInteger|softDeletes|softDeletesTz|string|text|time|timeTz|timestamp|timestampTz|tinyIncrements|tinyInteger|tinyText|unsignedBigInteger|unsignedDecimal|unsignedInteger|unsignedMediumInteger|unsignedSmallInteger|unsignedTinyInteger|uuid|year)(\?|(\*[^,]*))?(,)?)+$/';
 
         $errors = [];
         $columnArray = explode(',', $columns);
